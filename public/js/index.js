bd71616b73bd6e9e4fcfd3718a7b405e78fd7977
sssmi <simeon.kerkola@gmail.com>
2017-08-16 19:01:06 +0300

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io()
 
 socket.on('connect', () => {
   console.log('Connected to server')
-
-  socket.emit('createMessage', {
-    from: 'This guy',
-    text: 'Im online!',
-  })
 })
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -29,6 +29,11 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log(message)
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime(),
+    })
   })
 
   socket.on('disconnect', () => {
