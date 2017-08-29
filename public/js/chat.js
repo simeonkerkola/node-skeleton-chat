455b307a35b5f9778dc664fc3c58d683622cdb33
sssmi <simeon.kerkola@gmail.com>
2017-08-29 20:01:11 +0300

    All room names are lower case

--- public/js/chat.js
+++ public/js/chat.js
@@ -28,6 +28,7 @@ function scrollToBottom() {
 socket.on('connect', function () {
   // grab that search string and return an object w/ name and room values
   var params = $.deparam(window.location.search)
+  params.room = params.room.toLowerCase()
 
   // Check that room where trying to join indeed exists
   socket.emit('join', params, function (err) {
